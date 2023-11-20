3e0eb33f68cff72a3c73a4ce92e864cdc3d0aa93
DorTeri <dortayari@gmail.com>
2023-11-20 13:59:58 +0200

    Added clrek auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
+import { ClerkProvider } from '@clerk/nextjs'
 import type { Metadata } from 'next'
 import { Inter, Montserrat } from 'next/font/google'
 import './globals.css'
 import Provider from './(Providers)/NextUiProvider'
 
+
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 
-const montserrat = Montserrat({subsets: ['latin'] , variable: '--font-montserrat'})
+const montserrat = Montserrat({ subsets: ['latin'], variable: '--font-montserrat' })
 
 export const metadata: Metadata = {
   title: 'Bitprompt marketplace',
@@ -18,12 +20,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={`${inter.variable} ${montserrat.variable}`}>
-        <Provider>
-          {children}
-        </Provider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.variable} ${montserrat.variable}`}>
+          <Provider>
+            {children}
+          </Provider>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
