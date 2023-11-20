06032082d20b3f84d40a1f180d925a0cdebda012
DorTeri <dortayari@gmail.com>
2023-11-20 10:55:09 +0200

    Added globals and fonts

---
+++ app/(Main)/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import Header from "@/components/Header"
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default page
---
+++ components/Header.tsx
@@ -0,0 +1,34 @@
+import Link from 'next/link'
+import React, { useState } from 'react'
+
+type Props = {}
+
+const Header = (props: Props) => {
+    const [active, setActive] = useState(false)
+
+    if (typeof window !== 'undefined') {
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 100) {
+                setActive(true)
+            } else {
+                setActive(false)
+            }
+        })
+    }
+    return (
+        <div className={`w-full p-5 border-b min-h-[60px] border-b-[#ffffff32]
+         transition-opacity ${active && 'fixed top-0- left-0 bg-[#00000] z-[9999]'}`}>
+            <div className="hidden md:w-[90%] mx-auto md:flex items-center justify-between">
+                <div>
+                    <Link href={"/"}>
+                        <h1 className='font-Inter text-3xl cursor-pointer'>
+                            <span className='text-[#64ff4c]'>Bit</span>prompt
+                        </h1>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
