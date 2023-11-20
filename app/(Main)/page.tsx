6067116bf81b43c7bf698930ff8c483b71bdfbed
DorTeri <dortayari@gmail.com>
2023-11-20 11:56:13 +0200

    Added photos to Hero

--- app/(Main)/page.tsx
+++ app/(Main)/page.tsx
@@ -1,13 +1,29 @@
 'use client'
+import React, { useState, useEffect } from "react"
 import Header from "@/components/Header"
+import Hero from "@/components/Route/Hero"
 
 type Props = {}
 
 const Page = (props: Props) => {
-  
+
+  const [isMounted, setIsMounted] = useState(false)
+
+  useEffect(() => {
+    if (!isMounted) {
+      setIsMounted(true)
+    }
+  }, [isMounted])
+
+  if (!isMounted) {
+    return null
+  }
+
+
   return (
     <div>
-      <Header activeItem={0}/>
+      <Header activeItem={0} />
+      <Hero />
     </div>
   )
 }
---
+++ components/Route/Hero.tsx
@@ -0,0 +1,84 @@
+import Image from 'next/image'
+import React from 'react'
+import line from "@/public/Assets/line.png"
+import Marquee from 'react-fast-marquee'
+import image1 from "@/public/Assets/image1.png"
+import image2 from "@/public/Assets/image2.png"
+import image3 from "@/public/Assets/image3.png"
+import image4 from "@/public/Assets/image4.png"
+import image5 from "@/public/Assets/image5.png"
+
+
+type Props = {}
+
+const rowOneImages = [
+    {
+        src: image1,
+    },
+    {
+        src: image2,
+    },
+    {
+        src: image3,
+    },
+    {
+        src: image4,
+    },
+    {
+        src: image5,
+    },
+]
+
+const Hero = (props: Props) => {
+    return (
+        <div className='w-full md:min-h-screen flex items-center justify-center'>
+            <div>
+                <h1 className='font-Montserrat text-4xl py-5 xl:text-7xl
+                 2xl:text-8xl font-[700] text-center xl:leading=[80px] 
+                 2xl:leading-[100px] sm:mt-20'>
+                    Make <span className='text-[#64ff4b]'>Ai Image</span> <br />
+                    With your <br /> Imagination
+                </h1>
+                <div className='md:mt-5'>
+                    <Image
+                        src={line}
+                        alt='line'
+                        className='absolute hidden md:block'
+                        width={2000}
+                        height={2}
+                    />
+                </div>
+                <div className="w-[100vw] mb-5 md:mb-20 relative">
+                    <div className="rotate-[-4deg] mt-10 md:mt-[6.5rem]">
+                        <Marquee>
+                            {rowOneImages.map((i, index) => (
+                                <Image
+                                    src={i.src}
+                                    key={index}
+                                    alt='images'
+                                    className='md:m-4 w-[200px] m-2 md:w-[500px] rounded-[20px]'
+                                    width={500}
+                                    height={300}
+                                />
+                            ))}
+                        </Marquee>
+                        <Marquee>
+                            {rowOneImages.map((i, index) => (
+                                <Image
+                                    src={i.src}
+                                    key={index}
+                                    alt='images'
+                                    className='md:m-4 w-[200px] m-2 md:w-[500px] rounded-[20px]'
+                                    width={500}
+                                    height={300}
+                                />
+                            ))}
+                        </Marquee>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
