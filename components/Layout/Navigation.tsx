e27ddc6a480997046dc4747d0cd3339645aef560
DorTeri <dortayari@gmail.com>
2023-11-23 09:28:42 +0200

    Added shop orders

--- components/Layout/Navigation.tsx
+++ components/Layout/Navigation.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from 'react'
 
 type Props = {
@@ -31,12 +32,12 @@ const Navigation = ({ activeItem }: Props) => {
     return (
         <div className='block md:flex'>
             {navItems.map((item, index) => (
-                <div key={item.title}>
+                <Link href={item.href} key={item.title}>
                     <h5 className={`inline-block md:px-4 xl:px-8 py-5 md:py-0 text-[18px] font-[500]
-                     font-Inter ${activeItem === index && 'text-[#6dff4b]'}`}>
+                     font-Inter ${activeItem === index && 'text-[#6dff4b]'} cursor-pointer`}>
                         {item.title}
                     </h5>
-                </div>
+                </Link>
             ))}
         </div>
     )
