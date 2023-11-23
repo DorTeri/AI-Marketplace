922a2a78a55248ddfcd7f67b4721416e9d616591
DorTeri <dortayari@gmail.com>
2023-11-23 18:57:47 +0200

    Finished before adding AI

--- app/(Shop)/my-shop/page.tsx
+++ app/(Shop)/my-shop/page.tsx
@@ -1,18 +1,26 @@
-'use client';
-import ShopSidebar from "@/components/Shop/ShopSidebar"
-type Props = {}
+import ShopSidebar from "@/components/Shop/ShopSidebar";
+import ShopRoot from "./_page";
+import { getUser } from "@/actions/user/getUser";
+import { getShopOrders } from "@/actions/orders/getShopOrders";
+import { getAllPromptsByShop } from "@/actions/shop/getAllPromptsByShop";
 
-const Page = (props: Props) => {
-    return (
-        <div className="flex w-full">
-            <div className="h-screen flex p-2 bg-[#111C42] md:w-[32%] 2xl:w-[17%]">
-                <ShopSidebar active={0}/>
-            </div>
-            <div className="md:w-[80%] 2xl:w-[83%]">
-                
-            </div>
-        </div>
-    )
-}
+type Props = {};
 
-export default Page
+const Page = async (props: Props) => {
+  const sellerId: any = await getUser();
+  const ordersData = await getShopOrders({ sellerId: sellerId?.user.id });
+  const promptsData = await getAllPromptsByShop();
+
+  return (
+    <div className="flex w-full">
+      <div className="h-screen flex p-2 bg-[#111C42] md:w-[20%] 2xl:w-[17%]">
+        <ShopSidebar active={0} />
+      </div>
+      <div className="md:w-[80%] 2xl:w-[83%]">
+        <ShopRoot ordersData={ordersData} promptsData={promptsData} />
+      </div>
+    </div>
+  );
+};
+
+export default Page;
