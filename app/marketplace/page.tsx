43e9c4623775f966e15af59f8c83e539b3ddc3a3
DorTeri <dortayari@gmail.com>
2023-11-23 11:59:10 +0200

    Edited pagination

--- app/marketplace/page.tsx
+++ app/marketplace/page.tsx
@@ -2,9 +2,11 @@ import { getAllPrompts } from "@/actions/prompts/getAllPrompts";
 import MarketPlaceRouter from "./_page";
 import { getUser } from "@/actions/user/getUser";
 
-const Page = async () => {
+const Page = async ({ searchParams }: { searchParams: { [key: string]: string | undefined } }) => {
+  const page = searchParams["page"] ?? "1"
+  const pageNumber = parseInt(page)
   const data = await getUser();
-  const promptsData = await getAllPrompts()  
+  const promptsData = await getAllPrompts(pageNumber)
 
   return (
     <div>
