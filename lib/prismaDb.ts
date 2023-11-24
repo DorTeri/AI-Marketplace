ae684b2228f305cdcdf801d22aae2033d265d29f
DorTeri <dortayari@gmail.com>
2023-11-24 13:10:33 +0200

    db

--- lib/prismaDb.ts
+++ lib/prismaDb.ts
@@ -1,7 +1,23 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = global.prismadb || new PrismaClient();
-console.log('process.env.NODE_ENV', process.env.NODE_ENV)
-if (process.env.NODE_ENV === "production") global.prismadb = prisma
+console.log('process.env.NODE_ENV', process.env.NODE_ENV);
 
-export default prisma
+// Add logging for the Prisma connection
+(async () => {
+    try {
+        console.log("Connecting to the database...");
+
+        // This line ensures that PrismaClient is connected to the database
+        await prisma.$connect();
+
+        console.log("Connected to the database!");
+    } catch (error) {
+        console.error("Error connecting to the database:", error);
+        process.exit(1); // Exit the process if the connection fails
+    }
+})();
+
+if (process.env.NODE_ENV === "production") global.prismadb = prisma;
+
+export default prisma;
