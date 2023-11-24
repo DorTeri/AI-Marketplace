eb4ec17060b38f87212ac70b233bbec7079baf79
DorTeri <dortayari@gmail.com>
2023-11-24 12:05:58 +0200

    console log

--- lib/prismaDb.ts
+++ lib/prismaDb.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = global.prismadb || new PrismaClient();
-
+console.log('process.env.NODE_ENV', process.env.NODE_ENV)
 if (process.env.NODE_ENV === "production") global.prismadb = prisma
 
 export default prisma
