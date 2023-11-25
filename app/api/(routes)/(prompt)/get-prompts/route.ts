7f2ae84042d2a943578acef54c156b7c0889ddb3
DorTeri <dortayari@gmail.com>
2023-11-25 15:41:05 +0200

    Debug

--- app/api/(routes)/(prompt)/get-prompts/route.ts
+++ app/api/(routes)/(prompt)/get-prompts/route.ts
@@ -13,52 +13,52 @@ export async function GET(req: NextRequest) {
     const pageSize = 8;
 
     const prompts: any = await prisma.prompts.findMany({
-      include: {
-        orders: true,
-        images: true,
-        reviews: true,
-        promptUrl: true,
-      },
-      where: {
-        status: "Live",
-      },
-      take: pageSize,
-      skip: (pageNumber - 1) * pageSize,
-      orderBy: {
-        createdAt: "desc",
-      },
+      // include: {
+      //   orders: true,
+      //   images: true,
+      //   reviews: true,
+      //   promptUrl: true,
+      // },
+      // where: {
+      //   status: "Live",
+      // },
+      // take: pageSize,
+      // skip: (pageNumber - 1) * pageSize,
+      // orderBy: {
+      //   createdAt: "desc",
+      // },
     });
 
-    const totalPrompts: any = await prisma.prompts.findMany({
-      where: {
-        status: "Live",
-      },
-      include: {
-        images: true,
-      },
-    });
+    // const totalPrompts: any = await prisma.prompts.findMany({
+    //   where: {
+    //     status: "Live",
+    //   },
+    //   include: {
+    //     images: true,
+    //   },
+    // });
 
-    if (prompts) {
-      for (const prompt of prompts) {
-        const shop = await prisma.shops.findUnique({
-          where: {
-            userId: prompt.sellerId,
-          },
-        });
-        prompt.shop = shop;
-      }
+    // if (prompts) {
+    //   for (const prompt of prompts) {
+    //     const shop = await prisma.shops.findUnique({
+    //       where: {
+    //         userId: prompt.sellerId,
+    //       },
+    //     });
+    //     prompt.shop = shop;
+    //   }
 
-      for (const prompt of totalPrompts) {
-        const shop = await prisma.shops.findUnique({
-          where: {
-            userId: prompt.sellerId,
-          },
-        });
-        prompt.shop = shop;
-      }
-    }
+    //   for (const prompt of totalPrompts) {
+    //     const shop = await prisma.shops.findUnique({
+    //       where: {
+    //         userId: prompt.sellerId,
+    //       },
+    //     });
+    //     prompt.shop = shop;
+    //   }
+    // }
 
-    return NextResponse.json("{check}");
+    return NextResponse.json({prompts });
   } catch (error) {
     console.error("Error in GET request:", error);
 
