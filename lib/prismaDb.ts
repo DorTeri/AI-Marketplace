618b0d14cc6149d31e4c1481c4e346a12e4cbc39
DorTeri <dortayari@gmail.com>
2023-11-25 14:31:11 +0200

    test

--- lib/prismaDb.ts
+++ lib/prismaDb.ts
@@ -18,6 +18,6 @@ console.log('process.env.NODE_ENV', process.env.NODE_ENV);
     }
 })();
 
-if (process.env.NODE_ENV === "production") global.prismadb = prisma;
+// if (process.env.NODE_ENV === "production") global.prismadb = prisma;
 
 export default prisma;
