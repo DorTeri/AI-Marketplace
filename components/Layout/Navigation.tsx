f90ddbc58e1cc032ce50c5125c277f862b4f7498
DorTeri <dortayari@gmail.com>
2023-11-26 23:46:58 +0200

    Fixed navigation

--- components/Layout/Navigation.tsx
+++ components/Layout/Navigation.tsx
@@ -1,46 +1,49 @@
 import Link from "next/link";
-import React from 'react'
+import React from "react";
 
 type Props = {
-    activeItem: number,
-}
+  activeItem: number;
+};
 
 const navItems = [
-    {
-        title: "Home",
-        href: "/"
-    },
-    {
-        title: "About",
-        href: "/about"
-    },
-    {
-        title: "Marketplace",
-        href: "/marketplace"
-    },
-    {
-        title: "Contact",
-        href: "/contact"
-    },
-    {
-        title: "Policy",
-        href: "/policy"
-    },
-]
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "About",
+    href: "/about",
+  },
+  {
+    title: "Marketplace",
+    href: "/marketplace",
+  },
+  {
+    title: "Contact",
+    href: "/contact",
+  },
+  {
+    title: "Policy",
+    href: "/policy",
+  },
+];
 
 const Navigation = ({ activeItem }: Props) => {
-    return (
-        <div className='block md:flex'>
-            {navItems.map((item, index) => (
-                <Link href={item.href} key={item.title}>
-                    <h5 className={`inline-block md:px-4 xl:px-8 py-5 md:py-0 text-[18px] font-[500]
-                     font-Inter ${activeItem === index && 'text-[#6dff4b]'} cursor-pointer`}>
-                        {item.title}
-                    </h5>
-                </Link>
-            ))}
-        </div>
-    )
-}
+  return (
+    <div className="block md:flex">
+      {navItems.map((item, index) => (
+        <Link key={item.title} href={item.href}>
+          <h5
+            className={`md:px-4 xl:px-8 py-5 md:py-0 text-[18px] font-[500] font-Inter ${
+              activeItem === index && "text-[#6dff4b]"
+            }`}
+          >
+            {item.title}
+          </h5>
+        </Link>
+      ))}
+    </div>
+  );
+};
 
-export default Navigation
+export default Navigation;
